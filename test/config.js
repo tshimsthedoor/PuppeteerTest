097bf56335ca57367f219374fcb6235144648edd
Tresor Tshimanga Kazadi <tshims79@gmal.com>
2022-06-23 15:14:02 +0200

    second commit

--- test/config.js
+++ test/config.js
@@ -1,11 +1,21 @@
 module.exports = ({
     local: {
         baseURL : 'http://localhost:8080/',
-        timeout: 5000,
         username: 'admin@gmail.com',
         password: 'admin',
-        launchOptions: {headless: false}
+        launchOptions: { headless: false },
+        timeout: 50000,
     },
-    // test: {},
-    // prod: {}
+    CI: {
+        baseURL : 'http://localhost:8080/',
+        username: 'admin@gmail.com',
+        password: 'admin',
+        launchOptions: { 
+            executablePath: process.env.PUPPETEER_EXEC_PATH,
+            headless: true,
+            args: ['--no-sandbox'],
+        },
+        timeout: 50000,
+    },
+    prod: {}
 })[process.env.TESTENV || 'local'];
